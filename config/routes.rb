72f07b87bbb8b105f713572651ab8924cd49c538
Ginevra Fontes Williams <ginevra.f.williams@gmail.com>
2020-04-12 20:13:55 -0300

    fixed margins about

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'photos', to: 'photos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
